abfb70bbcb77eb554aeccaf3e81165a9f2e73a1a
Nathan Blanch <nldblanch@gmail.com>
2025-05-09 10:36:39 +0100

    init project

---
+++ src/main/java/com/v1/cacs_checklist/CacsChecklistApplication.java
@@ -0,0 +1,13 @@
+package com.v1.cacs_checklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacsChecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacsChecklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/v1/cacs_checklist/CacsChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.cacs_checklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacsChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
