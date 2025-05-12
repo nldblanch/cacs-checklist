0034d9ff72e5f34121f6e3cacad9f863ab4dc841
Charlie Worthington <charlie.worthington@version1.com>
2025-05-12 11:17:47 +0100

    add spring security

---
+++ src/main/java/com/v1/cacs_checklist/configs/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.v1.cacs_checklist.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.Customizer; // Correct import
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(4);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+
+                        //Admin permissions
+                        .requestMatchers("/admin/dashboard").hasAuthority("ADMIN")
+                        .requestMatchers("/admin/assign").hasAuthority("ADMIN")
+                        .requestMatchers("/admin/projects").hasAuthority("ADMIN")
+                        //Submitter permissions
+                        .requestMatchers("/submitter/dashboard").hasAuthority("SUBMITTER")
+                        .requestMatchers("/submitter/checklists").hasAuthority("SUBMITTER")
+                        .requestMatchers("/submitter/checklists/{id}").hasAuthority("SUBMITTER")
+                        .requestMatchers("/submitter/checklists/pending").hasAuthority("SUBMITTER")
+                        .requestMatchers("/submitter/checklists/pending/{id}").hasAuthority("SUBMITTER")
+                        //Assessor permissions
+                        .requestMatchers("/assessor/dashboard").hasAuthority("ASSESSOR")
+                        .requestMatchers("/assessor/checklists").hasAuthority("ASSESSOR")
+                        .requestMatchers("/assessor/checklists/{id}").hasAuthority("ASSESSOR")
+                        .requestMatchers("/assessor/checklists/pending").hasAuthority("ASSESSOR")
+                        .requestMatchers("/assessor/checklists/pending/{id}").hasAuthority("ASSESSOR")
+                        //Owner permissions
+                        .requestMatchers("/owner/dashboard").hasAuthority("OWNER")
+                        .requestMatchers("/owner/checklists").hasAuthority("OWNER")
+                        .requestMatchers("/owner/checklists/{id}").hasAuthority("OWNER")
+                        .requestMatchers("/owner/checklists/{id}/submissions").hasAuthority("OWNER")
+                        .requestMatchers("/owner/checklists/{id}/submissions/{id}").hasAuthority("ASSESSOR")
+                        // Other permissions
+                        .requestMatchers("/**").denyAll()
+                        .requestMatchers("/login.html").permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+
+    }
+}
---
+++ src/main/java/com/v1/cacs_checklist/services/UserService.java
@@ -0,0 +1,47 @@
+package com.v1.cacs_checklist.services;
+
+import com.v1.cacs_checklist.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsManager {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(
+                        ()-> new UsernameNotFoundException("No user found with username = " + username));
+    }
+
+    @Override
+    public void createUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void updateUser(UserDetails user) {
+
+    }
+
+    @Override
+    public void deleteUser(String username) {
+
+    }
+
+    @Override
+    public void changePassword(String oldPassword, String newPassword) {
+
+    }
+
+    @Override
+    public boolean userExists(String username) {
+//    if user exists return true else:
+        return false;
+    }
+}
