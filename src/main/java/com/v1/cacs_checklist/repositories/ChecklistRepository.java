3f7ab9cdfbf481d70f40e6ae13b220f50b0cb652
Charlie Worthington <charlie.worthington@version1.com>
2025-05-14 11:26:51 +0100

    redirect recent submissions url for submitter to a html page that displays the submitted checklist with a back button to return to the dashboard

--- src/main/java/com/v1/cacs_checklist/repositories/ChecklistRepository.java
+++ src/main/java/com/v1/cacs_checklist/repositories/ChecklistRepository.java
@@ -4,11 +4,13 @@ import com.v1.cacs_checklist.models.Checklist;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChecklistRepository extends MongoRepository<Checklist, String> {
     List<Checklist> findByOwnerEmail(String ownerEmail);
     List<Checklist> findBySubmitterEmail(String ownerEmail);
     List<Checklist> findByAssessorEmail(String ownerEmail);
+    Optional<Checklist> findByChecklistId(String checklistId);
     List<Checklist> findByOwnerEmailAndChecklistTemplateId(String ownerEmail, String checklistTemplateId);
 
 }
