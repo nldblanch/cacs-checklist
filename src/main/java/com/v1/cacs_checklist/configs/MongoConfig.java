a2e9feb7d85c9afa030f6915957ea1f0f54b7efc
Nathan Blanch <nldblanch@gmail.com>
2025-05-20 11:42:56 +0100

    add mongo repository template

--- src/main/java/com/v1/cacs_checklist/configs/MongoConfig.java
+++ src/main/java/com/v1/cacs_checklist/configs/MongoConfig.java
@@ -1,10 +1,17 @@
 package com.v1.cacs_checklist.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.v1.cacs_checklist.repositories.mongo")
 public class MongoConfig {
-}
 
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTemplate(mongoDatabaseFactory);
+    }
+}
